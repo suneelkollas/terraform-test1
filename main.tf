551d3a30aa37360887e33cd9731736404b3f6d3d
root <root@ip-172-31-38-190.ap-south-1.compute.internal>
2023-11-14 10:17:26 +0000

    terracommits

---
+++ main.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+region = "ap-south-1"
+}
+
+
+
+resource "aws_instance" "one" {
+ami = "ami-0a6ed6689998f32a5"
+instance_type = "t2.micro"
+key_name = "suneel_AWS_Key"
+availability_zone = "ap-south-1a"
+user_data	= <<EOF
+#!/bin/bash
+sudo -i
+yim install httpd -y
+systemctl start httpd
+chkconfig httpd on
+echo "Hi this is my means suneel website" > /var/www/html/index.html
+EOF
+tags = {
+Name = "Linux machine1"
+}
+}
+
+
+resource "aws_instance" "two" {
+ami = "ami-0a6ed6689998f32a5"
+instance_type = "t2.micro"
+key_name = "suneel_AWS_Key"
+availability_zone = "ap-south-1b"
+user_data       = <<EOF
+#!/bin/bash
+sudo -i
+yim install httpd -y
+systemctl start httpd
+chkconfig httpd on
+echo "Hi this is my means suneel website" > /var/www/html/index.html
+EOF
+tags = {
+Name = "Linux machine2"
+}
+}
+
